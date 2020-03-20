69a272fd370fa811065f23f2b3ee1f4ad6ccd973
Roshan Pandey <pandeyroshan556@gmail.com>
2020-03-20 23:12:08 +0530

    Deployment Settings

--- Flexshooz/settings.py
+++ Flexshooz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ti3dm2%11zx&d+vsuxb8(fgy0!oy!p906*6eb1f-y!z$tw4(5c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
