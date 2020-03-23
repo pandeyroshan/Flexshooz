9e219be71a065232336fb698e79a05ec65dc3d5f
Roshan Pandey <pandeyroshan556@gmail.com>
2020-03-23 18:08:15 +0530

    Static files URL

--- Flexshooz/settings.py
+++ Flexshooz/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'homepage'
 LOGIN_URL = 'login'
