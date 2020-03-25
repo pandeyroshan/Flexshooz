16ccd05a771ad3df772e0cb583cb6fccfd672ea1
Roshan Pandey <pandeyroshan556@gmail.com>
2020-03-25 23:57:37 +0530

    URLs for Cart, Wishlist

--- Flexshooz/urls.py
+++ Flexshooz/urls.py
@@ -5,11 +5,18 @@ from management import views as manage_view
 from users import views as user_view
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',store_view.index,name='homepage'),
-    path('login/',user_view.login,name='login')
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('register/',user_view.register,name='register'),
+    path('logout/',auth_views.LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL),name='logout'),
+    path('add_to_cart/<int:p_id>',store_view.add_to_cart,name='add_to_cart'),
+    path('add_to_wishlist/<int:p_id>',store_view.add_to_wishlist,name='add_to_wishlist'),
+    path('remove_from_cart/<int:p_id>',store_view.remove_from_cart,name='remove_from_wishlist'),
+    path('remove_from_wishlist/<int:p_id>',store_view.remove_from_wishlist,name='remove_from_wishlist'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
