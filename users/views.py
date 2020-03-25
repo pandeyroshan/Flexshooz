c9a778ec0ad35d4fc5f18906b0f6e595225fde65
Roshan Pandey <pandeyroshan556@gmail.com>
2020-03-26 00:00:27 +0530

    Register view

--- users/views.py
+++ users/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from .models import cart,wishList
 
 # Create your views here.
 
-def login(request):
-    if request.method=='POST':
-        print(request.POST)
-        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-        if user:
-            return redirect(request.META.get('HTTP_REFERER'))
-    return render(request,'users/login.html')
+def register(request):
+    if request.method == 'POST':
+        if request.POST.get('password') == request.POST.get('cnf_password'):
+            user = User.objects.create_user(request.POST.get('username'), request.POST.get('email'), request.POST.get('password'))
+            user.save()
+            user_cart = cart.objects.create(user=user)
+            user_cart.save()
+            user_wishlist = wishList.objects.create(user=user)
+            user_wishlist.save()
+            return redirect('/login')
+        else:
+            message = 'Password are not same'
+    return render(request,'users/register.html')
