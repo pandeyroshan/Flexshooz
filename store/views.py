28bed81b29072f7a7151ef8e59bbba2344f56644
Roshan Pandey <pandeyroshan556@gmail.com>
2020-03-27 13:49:23 +0530

    views for cart API

--- store/views.py
+++ store/views.py
@@ -30,19 +30,33 @@ def add_to_wishlist(request,p_id):
 
 # remove from cart
 @login_required
-def remove_from_cart(request,p_id):
-    product_object = Product.objects.get(id=p_id)
+def remove_from_cart(request,id):
+    product_object = Product.objects.get(id=id)
     cart_object = cart.objects.get(user=request.user)
     cart_object.productID.remove(product_object)
     cart_object.save()
-    return redirect('/')
+    return redirect('/cart')
 
 
 # remove from wishlist
 @login_required
-def remove_from_wishlist(request,p_id):
-    product_object = Product.objects.get(id=p_id)
+def remove_from_wishlist(request,id):
+    product_object = Product.objects.get(id=id)
     wishlist_object = wishList.objects.get(user=request.user)
     wishlist_object.productID.remove(product_object)
     wishlist_object.save()
     return redirect('/')
+
+def get_cart(request):
+    cartObject = cart.objects.filter(user=request.user).values("productID")
+    myList = []
+    try:
+        for data in cartObject:
+            id = data['productID']
+            myList.append(Product.objects.get(id=id))
+    except:
+        pass
+    price = 0
+    for data in myList:
+        price+=data.price
+    return render(request,'store/cart.html',{'items': myList,'price':price})
